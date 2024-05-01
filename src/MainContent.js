36a15ef02bce67e0c4619d18284b0120ed5e94be
Dandrev Nasi <97715883+dandrevn@users.noreply.github.com>
2024-05-01 15:14:22 +0800

    About me section

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,11 +2,10 @@
 import introPic from './imgs/meDP.jpg';
 const MainContent = () => {
     return (
-        <div className="h-full bg-blue-950 p-10">
-            <div className='main-content-wrapper flex flex-col'>
-                <div className='introduction-wrapper flex flex-col md:flex-row gap-3'>
+        <div className='main-content-wrapper flex flex-col gap-5 bg-sky-400'>
+                <div className='introduction-wrapper flex flex-col md:flex-row gap-3 p-10'>
                     <div className="image-content w-full flex justify-center md:w-1/2">
-                        <img className="object-cover rounded-lg w-full" src={introPic}/>
+                        <img className="object-cover w-full border-4 rounded-full" src={introPic}/>
                     </div>
                     <div className='text-content-wrapper flex flex-col gap-5 w-full md:w-1/2 md:justify-center'>
                         <div className='greetings-wrapper flex flex-col gap-2'>
@@ -22,9 +21,9 @@ const MainContent = () => {
                             </p>
                         </div>
                         <div className='nav-buttons-wrapper flex flex-col gap-3 md:flex-wrap md:flex-row md:justify-center'>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
+                            <a href='#aboutme'className='bg-transparent border text-white text-center p-1 w-full md:w-1/3'>
                                     About Me
-                            </button>
+                            </a>
                             <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
                                     Education 
                             </button>
@@ -43,7 +42,18 @@ const MainContent = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className='about-me-wrapper p-10 flex flex-col self-center h-full' id='aboutme'>
+                  <fieldset className='border border-solid p-5'>
+                    <legend className='text-5xl text-white'>ABOUT ME</legend>
+                    <p className='text-xl text-white text-justify'>
+                                Hi! I'm a goal-oriented Computer Science graduate from Polytechnic University of the Philippines.
+                            I'm currently looking for opportunities where I can expand and enhance my career as a Developer and as an IT Professional.
+                            I specialized mainly in Web Development utilizing web development technologies such as HTML, Javascript, CSS, and PHP. 
+                            I have been a web developer for one (1) year and five (5) months and I have used the content management system (CMS) called 
+                            WordPress. I'm also open to other roles that will allow to expand and improve my career such as Software Tester, IT Specialist, etc.
+                        </p>
+                  </fieldset>
+                </div>
         </div>
     );
 };
