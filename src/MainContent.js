bf7569fc255173825146fddcaf8d5740b4b4dfd3
Dandrev Nasi <97715883+dandrevn@users.noreply.github.com>
2024-05-02 18:42:50 +0800

    change color theme and some content

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,52 +1,56 @@
 //Portfolio Main Content Component
-import introPic from './imgs/meDP.jpg';
+import phpLogo from './imgs/php.png';
+import jsLogo from './imgs/js.png';
+import htmlLogo from './imgs/html.png';
+import cssLogo from './imgs/css.png';
+import wpLogo from './imgs/wp.png';
 const MainContent = () => {
     return (
-        <div className='main-content-wrapper flex flex-col gap-5 bg-sky-400'>
-                <div className='introduction-wrapper flex flex-col md:flex-row gap-3 p-10'>
-                    <div className="image-content w-full flex justify-center md:w-1/2">
-                        <img className="object-cover w-full border-4 rounded-full" src={introPic}/>
-                    </div>
-                    <div className='text-content-wrapper flex flex-col gap-5 w-full md:w-1/2 md:justify-center'>
-                        <div className='greetings-wrapper flex flex-col gap-2'>
-                            <p className="text-5xl text-white text-center md:text-left md:text-7xl">
-                                DANDREV NASI 
+        <div className='main-content-wrapper gap-10 bg-slate-200 p-5 md:p-10 h-auto'>
+                <div className='introduction-wrapper flex flex-col md:flex-row gap-3'>
+                    <div className='text-content-wrapper flex flex-col gap-5  p-4 w-full md:justify-center'>
+                        <div className='greetings-wrapper flex flex-col gap-10'>
+                            <p className="text-2xl text-justify md:text-4xl font-thin">
+                                    Greetings! 
                             </p>
-                            <p className="text-2xl text-white text-center md:text-left md:text-4xl">
-                                Web Developer
+                            <p className="text-2xl text-black text-justify md:text-4xl font-thin">
+                                    I'm Dandrev Nasi, a Web Developer.
                             </p>
-                            <p className="text-xl text-white text-justify md:text-2xl">
-                                Greetings! I'm a Web Developer, started my career as a WordPress developer. I'm a Computer Science graduate from
-                                Polytechnic University of the Philippines. Lorem lorem sinta ipsum ng papaya. Dadalay sudo sisidlan ng Django.
+                            <p className="text-2xl text-black text-justify md:text-4xl font-thin">
+                                    I have previously worked as a WordPress Developer.
                             </p>
-                        </div>
-                        <div className='nav-buttons-wrapper flex flex-col gap-3 md:flex-wrap md:flex-row md:justify-center'>
-                            <a href='#aboutme'className='bg-transparent border text-white text-center p-1 w-full md:w-1/3'>
-                                    About Me
-                            </a>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
-                                    Education 
-                            </button>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
-                                    Experiences
-                            </button>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
-                                    Tech stack
-                            </button>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
-                                    Connect with me!
-                            </button>
-                            <button type="button" className='bg-transparent border text-white p-1 w-full md:w-1/3'>
-                                    Works
-                            </button>
                         </div>
                     </div>
                 </div>
-                <div className='about-me-wrapper p-10 flex flex-col self-center h-full' id='aboutme'>
-                  <fieldset className='border border-solid p-5'>
-                    <legend className='text-5xl text-white'>ABOUT ME</legend>
-                    <p className='text-xl text-white text-justify'>
-                                Hi! I'm a goal-oriented Computer Science graduate from Polytechnic University of the Philippines.
+                <div className='tech-stack-wrapper'>
+                    <div className='ts-title'>
+                        <p className='text-3xl text-black font-thin'>
+                            TECH STACK
+                        </p>
+                    </div>
+                    <div className='marquee-wrapper flex flex-col md:flex-row justify-center gap-5'>
+                        <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                            <img src={htmlLogo} style={{height: '200px', width: '200px'}} />
+                        </div>
+                        <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                            <img src={cssLogo} style={{height: '200px', width: '200px'}} />
+                        </div>
+                        <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                            <img src={jsLogo} style={{height: '200px', width: '200px'}} className='' />
+                        </div>
+                        <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                            <img src={phpLogo} style={{height: '200px', width: '200px'}} />
+                        </div>    
+                        <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                            <img src={wpLogo} style={{height: '200px', width: '200px'}} />
+                        </div>       
+                    </div>
+                </div>
+                <div className='about-me-wrapper flex flex-col self-center' id='aboutme'>
+                  <fieldset className='border border-solid border-black p-5'>
+                    <legend className='text-3xl text-black font-thin'>ABOUT ME</legend>
+                    <p className='text-xl text-black text-justify font-thin'>
+                            I'm a goal-oriented Computer Science graduate from Polytechnic University of the Philippines.
                             I'm currently looking for opportunities where I can expand and enhance my career as a Developer and as an IT Professional.
                             I specialized mainly in Web Development utilizing web development technologies such as HTML, Javascript, CSS, and PHP. 
                             I have been a web developer for one (1) year and five (5) months and I have used the content management system (CMS) called 
